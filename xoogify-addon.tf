c9bec42b9a7240ee1a3f4adadd18816bc3828c8c
Marko Vlahovic <vlahovic74@gmail.com>
2023-09-06 10:03:38 -0700

    Add oidc configuration

--- xoogify-addon.tf
+++ xoogify-addon.tf
@@ -3,3 +3,105 @@ resource "aws_servicecatalog_principal_portfolio_association" "example" {
   principal_arn = module.aft_iam_roles.ct_management_exec_role_arn
   principal_type = "IAM"
 }
+
+data "google_client_config" "current" {}
+
+resource "aws_iam_openid_connect_provider" "xoogify" {
+  url             = "https://securetoken.google.com/${data.google_client_config.current.project}"
+  client_id_list  = [data.google_client_config.current.project]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+    "08745487e891c19e3078c1f2a07e452950ef36f6"
+  ]
+  tags = {
+    AddonName = "xoogify"
+  }
+}
+
+data "aws_iam_policy_document" "xoogify_oidc" {
+  statement {
+    sid    = "XoogifyOidcAuth"
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["${aws_iam_openid_connect_provider.xoogify.arn}/${aws_iam_openid_connect_provider.xoogify.url}"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${aws_iam_openid_connect_provider.xoogify.url}:aud"
+      values   = ["${data.google_client_config.current.project}"]
+    }
+
+    dynamic "condition" {
+      for_each = length(toset(var.xoogify_organizations)) > 0 ? [1] : []
+      content {
+        test     = "StringEquals"
+        variable = "${aws_iam_openid_connect_provider.xoogify.url}:organization"
+        values   = [toset(var.xoogify_organizations)]
+      }
+    }
+
+    dynamic "condition" {
+      for_each = length(toset(var.xoogify_uids)) > 0 ? [1] : []
+      content {
+        test     = "StringEquals"
+        variable = "${aws_iam_openid_connect_provider.xoogify.url}:uid"
+        values   = [toset(var.xoogify_uids)]
+      }
+    }
+
+    dynamic "condition" {
+      for_each = length(toset(var.xoogify_architectures)) > 0 ? [1] : []
+      content {
+        test     = "StringEquals"
+        variable = "${aws_iam_openid_connect_provider.xoogify.url}:architecture"
+        values   = [toset(var.xoogify_architectures)]
+      }
+    }
+  }
+}
+
+resource "aws_iam_role" "xoogify_oidc" {
+  name        = var.xoogify_name
+  path        = var.xoogify_path
+  description = "Role that allows xoogify hyperautomation to use OIDC to authenticate users and assume AFT roles"
+
+  assume_role_policy    = data.aws_iam_policy_document.xoogify_oidc.json
+  max_session_duration  = var.xoogify_max_session_duration
+  permissions_boundary  = var.xoogify_permissions_boundary_arn
+  force_detach_policies = var.xoogify_force_detach_policies
+
+  tags = {
+    "Addon" = "xoogify"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "xoogify-oidc-policy-attachment" {
+  role       = aws_iam_role.xoogify_oidc.name
+  policy_arn = aws_iam_policy.xoogify-policy.arn
+}
+
+data "aws_iam_policy_document" "xoogify-policy-definition" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = ["arn:aws:iam::*:role/AWSAFTAdmin"]
+  }
+}
+
+resource "aws_iam_policy" "xoogify-policy" {
+  name        = "xoogify-aft-hyperautomation-policy"
+  description = "Policy used by xoogify automation for admin access"
+  policy      = data.aws_iam_policy_document.xoogify-policy-definition.json
+}
+
+output "xoogify_hyperautomation_role_arn" {
+  description = "The ARN of the role that allows xoogify hyperautomation to use OIDC to authenticate users and assume AFT roles"
+  value       = aws_iam_role.xoogify_oidc.arn
+}
--- xoogify-variables.tf
+++ xoogify-variables.tf
@@ -2,3 +2,67 @@ variable portfolio_id {
   type = string
   description = "The ID of the portfolio to which to associate the principal."
 }
+
+################################################################################
+# GitHub OIDC Role
+################################################################################
+
+variable "xoogify_name" {
+  description = "Name of IAM role"
+  type        = string
+  default     = "xoogify-oidc-hyperautomation"
+}
+
+variable "xoogify_path" {
+  description = "Path of IAM role"
+  type        = string
+  default     = "/"
+}
+
+variable "xoogify_permissions_boundary_arn" {
+  description = "Permissions boundary ARN to use for IAM role"
+  type        = string
+  default     = null
+}
+
+variable "xoogify_description" {
+  description = "IAM Role description"
+  type        = string
+  default     = "Role used by hypetautomation"
+}
+
+variable "xoogify_name_prefix" {
+  description = "IAM role name prefix"
+  type        = string
+  default     = null
+}
+
+variable "xoogify_force_detach_policies" {
+  description = "Whether policies should be detached from this role when destroying"
+  type        = bool
+  default     = true
+}
+
+variable "xoogify_max_session_duration" {
+  description = "Maximum CLI/API session duration in seconds between 3600 and 43200"
+  type        = number
+  default     = null
+}
+
+variable "xoogify_organizations" {
+  description = "List of xoogify organizations that are allowed to assume xoofigy hyperautomation role"
+  type        = list(string)
+  default     = []
+}
+
+variable "xoogify_uids" {
+  description = "List of xoogify user ids that are allowed to assume xoofigy hyperautomation role"
+  type        = list(string)
+  default     = []
+}
+
+variable "xoogify_architectures" {
+  description = "List of xoogify architectures that are allowed to assume xoofigy hyperautomation role"
+  type        = list(string)
+  default     = []
+}
